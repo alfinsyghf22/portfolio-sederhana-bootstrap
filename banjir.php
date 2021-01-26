0563f56a84dbca47e43feae3c83e882b9817b119
alfinsyghf22 <71969291+alfinsyghf22@users.noreply.github.com>
2021-01-26 12:49:10 +0700

    Add files via upload

---
+++ banjir.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+    
+
+    <!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <div class="container">
+  <a class="navbar-brand" href="#">Dinas Komunikasi Informatika dan Statistika</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Utama <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontak</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tentang</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+<div style="text-align: center;">
+<img class="gambar" src="banjir.jpg">
+<br>
+<br>
+<h3><b>CIREBON</b> - Hujan deras yang mengguyur selama lima jam mengakibatkan lima kecamatan di Kabupaten Cirebon Jawa Barat terendam banjir , Minggu dinihari (27/12/2020). Ketinggian air di beberapa titik bahkan mencapai 1,5 meter selain intensitas hujan yang tinggi banjir juga disebabkan pendangkalan dan meluapnya sungai setelah mendapatkan kiriman dari wilayah hulu Kabupaten Kuningan.<br>
+<br>Ke lima kecamatan di Kabupaten Cirebon yang terendam banjir tersebut yakni Waled, Lemahabang, Astanajapura, Karangsembung dan Pangenan.
+<br>
+<br>Titik banjir terparah dialami Desa Mekarsari dan Desa Gunungsari di Kecamatan Waled dengan ketinggian air mencapai 1,5 meter.
+Menurut Herman Warga Waled, Kabupaten Cirebon banjir langganan di Desa Gunungsari ini sudah dirasakan sejak Sabtu malam.
+“Arus deras air dengan cepat masuk ke rumah-rumah warga setelah Sungai Ciberes yang mendapat kiriman air dari wilayah hulu meluap. Warga yang telah membendung pintu masuk dan lubang selokan tak bisa berbuat banyak setelah banjir rembes dan merendam rumah mereka,” kata Herman.
+<br>
+<br>Kepala BPBD Kabupaten CirebonAlex Suheriawan mengatakan, dari data sementara Badan Penanggulangan Bencana Daerah Kabupaten Cirebon banjir yang melanda Desa Gungusari ini merendam sedikitnya 350 rumah warga dengan ketinggian bervariasi dari 80-hingga 1,5 meter. <br>
+<br>Sementara dari lima kecamatan yang terendam hampir 2.000 rumah yang terendam
+“Sampai saat ini kondisi banjir masih merendam pemukiman warga pun khawatir banjir akan tinggi kembali jika hujan mengguyur dan kiriman air dari hulu tinggi,” kata Alex Suheriawan.
+<br>
+<br>
+<div class="">
+<div class="footer">
+  <p class="copy">Copyright DKIS 2021</p>
